a0cd1fafbf9e463e7621e03689537dc9f854a74e
Spydermunkey <putrid.rubbish@gmail.com>
2016-03-12 23:18:45 -0800

    Renamed "Gilded-Sabre" to "Gilded-Lance".

---
+++ Gilded-Lance/Assets/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public bool useController = false;
+    public float speed = 7.5F;
+
+    void Update()
+    {
+        transform.position += (transform.forward * Input.GetAxis("Zed")) * speed;
+        transform.position += (transform.right * Input.GetAxis("Ecks")) * speed;
+    }
+}
---
+++ Gilded-Lance/Assets/PlayerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Vector2 lookSensitivity = new Vector2(5, 5);
+
+    private Transform pcam;
+    private Transform player;
+    private Vector3 storage = new Vector3(0, 0, 0);
+
+
+    void Start()
+    {
+        player = this.transform;
+        pcam = player.GetChild(0);
+    }
+
+    void Update()
+    {
+        storage.x -= Input.GetAxis("Mouse Y") * lookSensitivity.y;
+        storage.y += Input.GetAxis("Mouse X") * lookSensitivity.x;
+
+        storage.x = Mathf.Clamp(storage.x, -70, 70);
+
+        transform.position += (transform.forward * Input.GetAxis("Zed")) * .25F;
+        transform.position += (transform.right * Input.GetAxis("Ecks")) * .25F;
+
+        player.rotation = Quaternion.Euler(0, storage.y, 0);
+        pcam.localRotation = Quaternion.Euler(storage.x, 0, 0);
+    }
+}
