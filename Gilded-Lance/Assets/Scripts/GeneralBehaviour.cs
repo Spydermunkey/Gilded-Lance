9d01cc94473b39ef980f3674d2d71f14d031c1cc
Lashon Tucker <Lashon Tucker>
2016-03-27 03:18:49 -0700

    Fixed mechanics and further optimized code

--- Gilded-Lance/Assets/Scripts/GeneralBehaviour.cs
+++ Gilded-Lance/Assets/Scripts/GeneralBehaviour.cs
@@ -8,35 +8,33 @@ public class GeneralBehaviour : MonoBehaviour
     private Transform pcam;
     private Transform player;
     private Vector2 storage;
-    // rip speed :(
-
-    //rip Use controller :(
-    // new vars 
+  
     private Transform cursorCube;
     private Vector3 posKeeper;
     private Vector3 otherRefVec;
     private float t = 0.2f;
-    //use debug inspector to view
-    private bool grounded;
+
     private Vector3 refVec;
     private Transform playerBlock;
     private float coolTime;
     private Transform trans;
+    private Camera overWatch;
 
     private Rigidbody rigid;
-    //rip jumptimer :(
     private float conductor;
-    public Vector3 jump = new Vector3(0, 5, 0); 
+    private Camera cam;
 
     void Start() {
         player = this.transform;
         pcam = player.GetChild(0);
         rigid = player.GetComponent<Rigidbody>();
         cursorCube = player.GetChild(1);
-        playerBlock = player.GetChild(2);
+        trans = player.GetChild(2).GetChild(0);
+        overWatch = player.GetChild(3).GetChild(0).GetComponent<Camera>();
         player.DetachChildren();
         pcam.parent = player;
-        trans = playerBlock.GetChild(0);
+        overWatch.transform.parent = player;
+        cam = trans.GetComponent<Camera>();
         
     }
 
@@ -44,34 +42,51 @@ public class GeneralBehaviour : MonoBehaviour
         conductor = Time.smoothDeltaTime;
         if (coolTime > 0)
             coolTime -= conductor;
+
         RaycastHit hit;
         if (Physics.Raycast(pcam.position, pcam.forward, out hit)){
-
-            cursorCube.position = Vector3.SmoothDamp(cursorCube.position, new Vector3(hit.point.x, hit.point.y + 1, hit.point.z), ref refVec, 0.1f);
+            Vector3 hitPos = hit.point;
+            cursorCube.position = Vector3.SmoothDamp(cursorCube.position, new Vector3(hitPos.x, hitPos.y + 1.2f, hitPos.z), ref refVec, 0.1f);
         }
-
+        
         if (Input.GetButtonDown("Fire1") && coolTime <= 0){
+            coolTime = 1.98f;
             posKeeper = cursorCube.position;
             rigid.useGravity = false;
         }
-        if (Input.GetButtonDown("Fire2")) { 
+        else if (Input.GetButtonDown("Fire2")) { 
         
-            playerBlock.position = cursorCube.position;
-            playerBlock.LookAt(player);
+            trans.position = cursorCube.position;
+            trans.LookAt(player);
         }
-        if (Input.GetButtonDown("Fire3"))
-            trans.GetComponent<Camera>().enabled = !trans.GetComponent<Camera>().enabled;
-
-        if (Input.GetAxis("Mouse ScrollWheel") != 0) {
-
-           trans.gameObject.SetActive(true);
-            Camera cam = pcam.GetComponent<Camera>();
-            cam.fieldOfView = 30f;
+        else if (Input.GetButtonDown("Fire3")) {
+           
+           cam.enabled = !cam.enabled;
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") != 0) {
+            Debug.Log("The Scroll doth wheel!");
+            if(cam == trans.GetComponent<Camera>()) {
+                Debug.Log("Sucess?");
+                cam.depth = -1;
+                cam.enabled = false;
+                cam = overWatch;
+                cam.enabled = true;
+                cam.depth = 2;
+            }
+            else if (cam == overWatch)
+            {
+                Debug.Log("Another Sucess?");
+                cam.depth = -1;
+                cam.enabled = false;
+                cam = trans.GetComponent<Camera>();
+                cam.enabled = true;
+                cam.depth = 2;
+            }
 
         }
 
         if (posKeeper != Vector3.zero) {
-            coolTime = 1.98f;
+
             player.position = Vector3.SmoothDamp(player.position, posKeeper, ref otherRefVec, 0.1f);
             t -= conductor;
             if (t < 0) {
@@ -81,9 +96,10 @@ public class GeneralBehaviour : MonoBehaviour
                 rigid.velocity = posKeeper;
                 t = 0.2f;
             }
+      
 
         }
-        Debug.DrawRay(pcam.position, -pcam.forward, Color.green);
+
         storage = new Vector2(Mathf.Clamp(storage.x - Input.GetAxis("Mouse Y") * lookSensitivity.y * conductor, -95, 85)
                                         , storage.y + Input.GetAxis("Mouse X") * lookSensitivity.x * conductor);
 
